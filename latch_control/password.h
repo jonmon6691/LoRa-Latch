18590b3509b7997ac9fb71a78ee3d7c5845520be
jonmon6691 <jonmon6691@gmail.com>
2020-11-08 14:24:06 -0800

    Better password handling

--- latch_control/password.h
+++
@@ -1,7 +0,0 @@
-#ifndef PASSWORD_H
-#define PASSWORD_H
-
-#define PASSWORD "change me"
-#define PASSWORD_LEN 9
-
-#endif
---
+++ set_password.sh
@@ -0,0 +1,11 @@
+echo -n "Password: " && read -s pw
+echo "#ifndef PASSWORD_H" > password.h
+echo "#define PASSWORD_H" >> password.h
+echo "#define PASSWORD \"$pw\"" >> password.h
+echo "#define PASSWORD_LEN ${#pw}" >> password.h
+echo "#endif" >> password.h
+cp password.h latch_control/
+cp password.h latch_remote/
+rm password.h
+echo
+echo " [DONE] Password set"
